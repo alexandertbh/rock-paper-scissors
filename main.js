ed4233989cadfee0a9cf2ac4090a6f7547ba7fd3
Alex Horning <68720206+alexandertbh@users.noreply.github.com>
2023-03-14 10:03:18 -0700

    adding changes after fixing git workflow

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
 console.log("Hi! Lets play Rock, Paper, Scissors!");
+const userInput = document.getElementById("#userInput");
+const computerResponse = document.querySelector('#computerResponse');
+const userResponse = document.querySelector("#userResponse");
+const winner = document.querySelector('#winner');
 
-
-function gameTime() {
-  let userInput = document.querySelector("#userInput").value;
-  let computerResponse = document.querySelector('#computerResponse');
-  let userResponse = document.querySelector("#userResponse");
-  let winner = document.querySelector('#winner');
-  userResponse.innerHTML = "User Input: " + userInput;
-  computerResponse.innerHTML='Computer Response: '+ getComputerChoice();
-  winner.innerHTML = 'The winner is:' + playGame();
-  
-  
-}
-
+console.log('user input'+ userInput);
 const getUserChoice = (userInput) => {
-  userInput = userInput.toLowerCase();
+  //userInput = userInput.toLowerCase();
   if (
     userInput === "rock" ||
     userInput === "paper" ||
@@ -26,7 +18,11 @@ const getUserChoice = (userInput) => {
     console.log("You must input Rock, Paper, Or Scissors");
   }
 };
-const getComputerChoice = () => {
+
+console.log(getUserChoice() + 'get user choice');
+console.log(userInput + 'user input');
+
+function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
     case 0:
@@ -36,7 +32,9 @@ const getComputerChoice = () => {
     case 2:
       return "scissors";
   }
-};
+}
+
+console.log(getComputerChoice() +' get computer choice');
 
 const determineWinner = (userChoice, computerChoice) => {
   if (userChoice === "bomb") {
@@ -68,16 +66,25 @@ const determineWinner = (userChoice, computerChoice) => {
     return "the computer won";
   }
 };
-
+console.log(getComputerChoice);
 const playGame = () => {
   const userChoice = getUserChoice(userInput);
   const computerChoice = getComputerChoice();
 
-  console.log("You threw " + userChoice);
+  /*console.log("You threw " + userChoice);
   console.log("The computer threw " + computerChoice);
-  return determineWinner(userChoice, computerChoice);
+  return determineWinner(userChoice, computerChoice);*/
 };
+
 
 console.log(playGame());
 
-console.log(userInput());
+console.log(userInput);
+
+
+function gameTime() {
+
+  userResponse.innerHTML = "User Input: " + getUserChoice();
+  computerResponse.innerHTML='Computer Response: '+ getComputerChoice();
+  winner.innerHTML = 'The winner is:' + determineWinner();
+}
